e8c3dd428b43ca1e5ba239efd64ddff9dcdbd59b
huanglr <963595002@qq.com>
2020-11-14 22:14:44 +0800

    test

---
+++ src/test/java/com/zmx/jta/test/test.java
@@ -0,0 +1,22 @@
+public class Response {
+
+    private String sCode2;
+
+    private String sMsg2;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
